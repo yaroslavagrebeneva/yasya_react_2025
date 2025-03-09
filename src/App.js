3060b81599febeaa55bd1cbb45995e4cba759bea
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-09 13:42:35 +0300

    19

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  const attr = 'block';
+  const li1 = <li>text1</li>;
+  const li2 = <li>text2</li>;
+  const li3 = <li>text3</li>;
 
-  return <div id={attr}>text</div>;
+  return (
+    <ul>
+      {li1}
+      {li2}
+      {li3}
+    </ul>
+  );
 }
-
-
 
 
 
