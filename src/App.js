e40eff6adf47a27bda4ebda683a51d0f1282ce8c
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-09 13:38:29 +0300

    task11

--- src/App.js
+++ src/App.js
@@ -1,41 +1,10 @@
 import './App.css';
 import React from 'react';
 
-//1 способ
 function App() {
-  return (
-    <>
-      <ul>
-        <li>text1</li>
-        <li>text2</li>
-        <li>text3</li>
-      </ul>
-      <ul>
-        <li>text1</li>
-        <li>text2</li>
-        <li>text3</li>
-      </ul>
-    </>
-  );
+  return <input />;
 }
 
-// 2 способ
-// function App() {
-//   return (
-//     <div>
-//       <ul>
-//         <li>text1</li>
-//         <li>text2</li>
-//         <li>text3</li>
-//       </ul>
-//       <ul>
-//         <li>text1</li>
-//         <li>text2</li>
-//         <li>text3</li>
-//       </ul>
-//     </div>
-//   );
-// }
 
 
 export default App;
