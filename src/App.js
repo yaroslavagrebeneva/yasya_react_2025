c76c89c9bf15fb5ef9684b59dde8e4b50ec4dd43
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-09 13:43:06 +0300

    20

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  const li1 = <li>text1</li>;
-  const li2 = <li>text2</li>;
-  const li3 = <li>text3</li>;
-
-  return (
-    <ul>
-      {li1}
-      {li2}
-      {li3}
-    </ul>
+  const items = (
+    <>
+      <li>text1</li>
+      <li>text2</li>
+      <li>text3</li>
+    </>
   );
+
+  return <ul>{items}</ul>;
 }
 
 
