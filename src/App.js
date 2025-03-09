d47528cbdfc97b74427e083607165d654f04ee3f
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-09 13:36:55 +0300

    task9

--- src/App.js
+++ src/App.js
@@ -4,18 +4,12 @@ import React from 'react';
 function App() {
   return (
     <ul>
-      <li>Item 1</li>
-      <li>Item 2</li>
-      <li>Item 3</li>
-      <li>Item 4</li>
-      <li>Item 5</li>
-      <li>Item 6</li>
-      <li>Item 7</li>
-      <li>Item 8</li>
-      <li>Item 9</li>
-      <li>Item 10</li>
+      <li>text1</li>
+      <li>text2</li>
+      <li>text3</li>
     </ul>
   );
 }
+
 
 export default App;
