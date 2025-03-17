581849f03dbb17da11545c1be2a9c4ab929ea19b
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-17 14:08:50 +0300

    task#40-41

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 function App() {
-  const arr = ['a', 'b', 'c', 'd', 'e'];  
+ 
+  const users = [
+    { name: 'user1', surn: 'surn1', age: 30 },
+    { name: 'user2', surn: 'surn2', age: 31 },
+    { name: 'user3', surn: 'surn3', age: 32 },
+  ];
 
   return (
     <div>
       <ul>
-        {arr.map((item, index) => (
-          <li key={index}>{item}</li>  
+        
+        {users.map((user, index) => (
+          <li key={index}>
+            Name: {user.name}, Surname: {user.surn}, Age: {user.age}
+          </li>
         ))}
       </ul>
     </div>
