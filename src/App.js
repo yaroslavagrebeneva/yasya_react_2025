38aad7d1765bf9ba6f6a361ec73bbf2bf0ee76f0
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-17 14:11:46 +0300

    task#49

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import { nanoid } from 'nanoid'; 
-import uuid from 'react-uuid';   
+import { nanoid } from 'nanoid';
+import uuid from 'react-uuid';
 
-// №1
-function idWithNanoid() {
-  return nanoid(); 
-}
-
-// №2
-function idWithReactUuid() {
-  return uuid(); 
+function generateId(usingNanoid = true) {
+  return usingNanoid ? nanoid() : uuid();
 }
 
 function App() {
-  const randomIdNanoid = idWithNanoid();  
-  const randomIdReactUuid = idWithReactUuid();  
+  const users = [
+    { id: generateId(true), name: 'User1', surname: 'Surname1', age: 30 },
+    { id: generateId(true), name: 'User2', surname: 'Surname2', age: 31 },
+    { id: generateId(true), name: 'User3', surname: 'Surname3', age: 32 },
+  ];
 
   return (
     <div>
-      <p>Случайный ID (nanoid): {randomIdNanoid}</p>
-      <p>Случайный UUID (react-uuid): {randomIdReactUuid}</p>
+      <h1>Users List (using nanoid)</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {user.name} {user.surname} - Age: {user.age} (ID: {user.id})
+          </li>
+        ))}
+      </ul>
+
+      <h1>Users List (using react-uuid)</h1>
+      <ul>
+        {users.map(user => (
+          <li key={generateId(false)}>
+            {user.name} {user.surname} - Age: {user.age} (ID: {generateId(false)})
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
