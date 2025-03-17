25d159f4e504e998df555116137c0cdaaab8d45d
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-17 14:12:48 +0300

    task#53

--- src/App.js
+++ src/App.js
@@ -3,21 +3,17 @@ import React, { useState } from 'react';
 function App() {
   const [name, setName] = useState('John');
   const [surname, setSurname] = useState('Doe');
-  const [age, setAge] = useState(30);
 
   const handleNameChange = () => setName('Alex');
   const handleSurnameChange = () => setSurname('Smith');
-  const handleAgeChange = () => setAge(35);
 
   return (
     <div>
       <p>Name: {name}</p>
       <p>Surname: {surname}</p>
-      <p>Age: {age}</p>
-      
+
       <button onClick={handleNameChange}>Change Name</button>
       <button onClick={handleSurnameChange}>Change Surname</button>
-      <button onClick={handleAgeChange}>Change Age</button>
     </div>
   );
 }
