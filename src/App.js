ad9bef136627675f82bb678c2687410b8025b3c3
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-17 13:56:05 +0300

    task#26

--- src/App.js
+++ src/App.js
@@ -1,35 +1,20 @@
 import React from 'react';
 
+
 function App() {
-  // №1
-  const num1 = 3;
-  const num2 = 2;
-
-  // №2
-  const name = 'john';
-  const surname = 'smit';
-
-  // №3
-  const num = 4;
+  const isAdult = true;
 
   return (
     <div>
-      {/* №1 */}
-      <div>
-        result: {num1 ** num2}
-      </div>
-
-      {/* №2 */}
-      <div>
-        result: {name + ' ' + surname}
-      </div>
-
-      {/* №3 */}
-      <div>
-        result: {Math.sqrt(num)}
-      </div>
+      {isAdult ? (
+        <p>Вы совершеннолетний.</p>
+      ) : (
+        <p>Вы несовершеннолетний.</p>
+      )}
     </div>
   );
 }
 
 export default App;
+
+
