3efd1e1e16d70e6225d6e6b234296c3489a6d7ed
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-17 14:11:18 +0300

    task#48

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import React from 'react';
 import { nanoid } from 'nanoid'; 
 import uuid from 'react-uuid';   
 
+// №1
+function idWithNanoid() {
+  return nanoid(); 
+}
+
+// №2
+function idWithReactUuid() {
+  return uuid(); 
+}
+
 function App() {
- 
-  const randomId = nanoid();  
-  const randomUuid = uuid();  
+  const randomIdNanoid = idWithNanoid();  
+  const randomIdReactUuid = idWithReactUuid();  
 
   return (
     <div>
-      <p>Случайный ID (nanoid): {randomId}</p>
-      <p>Случайный UUID (react-uuid): {randomUuid}</p>
+      <p>Случайный ID (nanoid): {randomIdNanoid}</p>
+      <p>Случайный UUID (react-uuid): {randomIdReactUuid}</p>
     </div>
   );
 }
