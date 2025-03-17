fb9c64e7a1907c1f7117aef33a112967a27713bf
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-17 14:04:37 +0300

    task#32

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
 function App() {
-  
   function getDigitsSum(number) {
     return number
-      .toString()       
-      .map(Number)       
+      .toString()         
+      .split('')          
+      .map(Number)        
       .reduce((sum, num) => sum + num, 0);  
   }
 
   
-  const sum = getDigitsSum(123);
+  const sum = getDigitsSum(12345);
 
   return (
     <div>
-      <p>Сумма цифр числа 123: {sum}</p>
+      <p>Сумма цифр числа 12345: {sum}</p>
     </div>
   );
 }
