815d5de2f3be8071e365afb8f418d088f3857457
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-17 14:12:15 +0300

    task#52

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import React from 'react';
-import { nanoid } from 'nanoid';
-import uuid from 'react-uuid';
-
-function generateId(usingNanoid = true) {
-  return usingNanoid ? nanoid() : uuid();
-}
+import React, { useState } from 'react';
 
 function App() {
-  const users = [
-    { id: generateId(true), name: 'User1', surname: 'Surname1', age: 30 },
-    { id: generateId(true), name: 'User2', surname: 'Surname2', age: 31 },
-    { id: generateId(true), name: 'User3', surname: 'Surname3', age: 32 },
-  ];
+  const [name, setName] = useState('John');
+  const [surname, setSurname] = useState('Doe');
+  const [age, setAge] = useState(30);
+
+  const handleNameChange = () => setName('Alex');
+  const handleSurnameChange = () => setSurname('Smith');
+  const handleAgeChange = () => setAge(35);
 
   return (
     <div>
-      <h1>Users List (using nanoid)</h1>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>
-            {user.name} {user.surname} - Age: {user.age} (ID: {user.id})
-          </li>
-        ))}
-      </ul>
-
-      <h1>Users List (using react-uuid)</h1>
-      <ul>
-        {users.map(user => (
-          <li key={generateId(false)}>
-            {user.name} {user.surname} - Age: {user.age} (ID: {generateId(false)})
-          </li>
-        ))}
-      </ul>
+      <p>Name: {name}</p>
+      <p>Surname: {surname}</p>
+      <p>Age: {age}</p>
+      
+      <button onClick={handleNameChange}>Change Name</button>
+      <button onClick={handleSurnameChange}>Change Surname</button>
+      <button onClick={handleAgeChange}>Change Age</button>
     </div>
   );
 }
