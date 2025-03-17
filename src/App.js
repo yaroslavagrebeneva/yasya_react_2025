28ed64eba017c80795f652023b9206b3cada1b9c
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-17 14:07:39 +0300

    task#38

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 
 function App() {
-  // Массив элементов li
-  const arr = [
-    <li key={1}>1</li>,
-    <li key={2}>2</li>,
-    <li key={3}>3</li>,
-    <li key={4}>4</li>,
-    <li key={5}>5</li>,
-  ];
+  const items = [];  
+
+ 
+  for (let i = 1; i <= 5; i++) {
+    items.push(<li key={i}>{i}</li>);
+  }
 
   return (
     <div>
       <ul>
-        {arr} {/* Вставляем массив элементов li в список ul */}
+        {items} 
       </ul>
     </div>
   );
