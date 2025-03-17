089ae9c1769b4840acf86ce186d1e4ce7148de0c
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-03-17 13:55:01 +0300

    task#24

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import './App.css';
 import React from 'react';
 
 function App() {
+  // №1
+  const num1 = 3;
+  const num2 = 2;
+
+  // №2
+  const name = 'john';
+  const surname = 'smit';
+
+  // №3
+  const num = 4;
+
   return (
     <div>
-      <input />
-      <br />
-      <input />
-      <br />
-      <input />
+      {/* №1 */}
+      <div>
+        result: {num1 ** num2}
+      </div>
+
+      {/* №2 */}
+      <div>
+        result: {name + ' ' + surname}
+      </div>
+
+      {/* №3 */}
+      <div>
+        result: {Math.sqrt(num)}
+      </div>
     </div>
   );
 }
 
-
-
-
 export default App;
-
-
--- src/App.test.js
+++ src/App.test.js
@@ -1,43 +1,8 @@
-import './App.css';
-import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-function App() {
-  // Массив с элементами для списка
-  const items = Array.from({ length: 10 }, (_, index) => (
-    <li key={index}>Item {index + 1}</li>
-  ));
-
-  return (
-    <div>
-      <h1>23_1</h1>
-      {/* Список */}
-      <ul>{items}</ul>
-
-      <h1>23_2</h1>
-      {/* Таблица */}
-      <table border="1">
-        <thead>
-          <tr>
-            <th>1,1</th>
-            <th>1,2</th>
-            <th>1,3</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>2,1</td>
-            <td>2,2</td>
-            <td>2,3</td>
-          </tr>
-          <tr>
-            <td>3,1</td>
-            <td>3,2</td>
-            <td>3,3</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-}
-
-export default App;
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
