c83f46c16d15f70887bbfbd06c94688b0eb672f9
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-04-10 18:18:34 +0300

    task#59

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [name, setName] = useState('John');
-  const [surname, setSurname] = useState('Doe');
+  const [numbers, setNumbers] = useState([0, 0, 0, 0, 0]);
 
-  const handleNameChange = () => setName('Alex');
-  const handleSurnameChange = () => setSurname('Smith');
+  const handleInputChange = (index, event) => {
+    const newNumbers = [...numbers];
+    newNumbers[index] = parseFloat(event.target.value) || 0;
+    setNumbers(newNumbers);
+  };
+
+  const average = numbers.reduce((acc, num) => acc + num, 0) / numbers.length;
 
   return (
     <div>
-      <p>Name: {name}</p>
-      <p>Surname: {surname}</p>
-
-      <button onClick={handleNameChange}>Change Name</button>
-      <button onClick={handleSurnameChange}>Change Surname</button>
+      <input
+        type="number"
+        value={numbers[0]}
+        onChange={(e) => handleInputChange(0, e)}
+      />
+      <input
+        type="number"
+        value={numbers[1]}
+        onChange={(e) => handleInputChange(1, e)}
+      />
+      <input
+        type="number"
+        value={numbers[2]}
+        onChange={(e) => handleInputChange(2, e)}
+      />
+      <input
+        type="number"
+        value={numbers[3]}
+        onChange={(e) => handleInputChange(3, e)}
+      />
+      <input
+        type="number"
+        value={numbers[4]}
+        onChange={(e) => handleInputChange(4, e)}
+      />
+      <p>Среднее арифметическое: {average}</p>
     </div>
   );
 }
