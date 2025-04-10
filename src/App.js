eacc935a5a1a028c065ebc1496b74ca2a28f0fcd
anarhisssttt <grebeneva.y.i.23@gmail.com>
2025-04-10 18:16:01 +0300

    task57

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, { useState } from 'react';
 
 function App() {
-  const [text1, setText1] = useState('');
-  const [text2, setText2] = useState('');
+  const [inputText, setInputText] = useState('');
 
   return (
     <div>
       <input 
         type="text" 
-        value={text1} 
-        onChange={event => setText1(event.target.value)} 
+        value={inputText} 
+        onChange={event => setInputText(event.target.value)} 
       />
-      <p>Text 1: {text1}</p>
-
-      <input 
-        type="text" 
-        value={text2} 
-        onChange={event => setText2(event.target.value)} 
-      />
-      <p>Text 2: {text2}</p>
+      <p>Количество символов: {inputText.length}</p>
     </div>
   );
 }
